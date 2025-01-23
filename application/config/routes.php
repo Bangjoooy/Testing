e440a16f9782b816ca66e030753d16ff1d53e533
bangjoooy <wijayarx@gmail.com>
2025-01-23 15:15:09 +0700

    upload index html dll

---
+++ application/config/routes.php
@@ -0,0 +1,2 @@
+$route['default_controller'] = 'welcome'; // Mengarah ke controller Welcome sebagai halaman utama
+$route['accounting'] = 'accounting'; // Mengarah ke controller Accounting untuk penginputan
